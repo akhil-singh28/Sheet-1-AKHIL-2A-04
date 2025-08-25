f017f69bc8a840c888694d65ca9bc69adebb64d4
akhil-singh28 <as1575828@gmail.com>
2025-08-25 17:56:24 +0530

    Update div7_last5.py

--- div7_last5.py
+++ div7_last5.py
@@ -4,4 +4,4 @@ num = int(input("Enter a number: "))
 if num % 7 == 0 or num % 10 == 5:
     print("The number is divisible by 7 OR last digit is 5")
 else:
-    print("Condition not satisfied")
+    print("Condition is FALSE")
