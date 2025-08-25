525ab3e9d1e04a84d7957c907792c8e188494061
akhil-singh28 <as1575828@gmail.com>
2025-08-25 17:56:00 +0530

    Update div3_last4.py

--- div3_last4.py
+++ div3_last4.py
@@ -4,4 +4,4 @@ num = int(input("Enter a number: "))
 if num % 3 == 0 and num % 10 == 4:
     print("The number is divisible by 3 and last digit is 4")
 else:
-    print("Condition not satisfied")
+    print("Condition IS FALSE")
